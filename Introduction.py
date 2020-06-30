b2a4be0e0e196d9da7c023914ba3efe086cfd817
playsam32 <wsung0011@naver.com>
2020-06-30 18:03:38 +0900

    First

---
+++ Introduction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+#  Copyright (c) 2020 by 5HAPZIJOL00, All rights reserved.
+#  (5HAPZIJOL description)
+#
+#  Project       : MenuKim
+#  Starting date : June. 30, 2020
+#
+#  Teammate      : WooSung Chung  (wsung0011@naver.com)
+#                : (username, email)
+#
+#  py version    : made by CPython 3.8.3, 64-bit
+#
+#  Modification History:
+#     * version 0.1, by 5HAPJIzol, Jun. 30, 2020
+#       - 1st released on this day.
+#
