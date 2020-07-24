9be38a3d0e6998d5d217d925debde180bac8ef3d
yunji118 <yunji118@naver.com>
2020-07-24 17:16:21 +0900

    Update sample.py

--- UNIVERSITY/Ewha Womans University/sample.py
+++ UNIVERSITY/Ewha Womans University/sample.py
@@ -4,6 +4,14 @@ write your docstring
 '''
 
 
-def sample():
-    """Delete this function. write your own code."""
-    pass
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+import pandas as pd
+import requests
+
+URL ='https://www.ewha.ac.kr/ewha/life/restaurant.do?mode=view&articleNo=905&article.offset=0&articleLimit=10&srDt=2020-07-20'
+html = urlopen(URL)
+BS = bs(html, "html.parser")
+menu = BS.find("", {"class":"b-menu b-menu-l lunch"})
